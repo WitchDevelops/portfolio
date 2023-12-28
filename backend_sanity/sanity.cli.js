3b8b4170d7654599ad26bb512c4b669af7d56cd1
WitchDevelops <d.n.wojewska@gmail.com>
2023-12-28 10:55:03 +0100

    initial commit, sanity setup

---
+++ backend_sanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'cnqvh2wp',
+    dataset: 'production'
+  }
+})
---
+++ backend_sanity/schemas/testimonials.js
@@ -0,0 +1,30 @@
+export default {
+  name: 'testimonials',
+  title: 'Testimonials',
+  type: 'document',
+  fields: [
+    {
+      name: 'name', //name of the person leaving the testimonial
+      title: 'Name', 
+      type: 'string',
+    },
+    {
+      name: 'company', //if they work for a company
+      title: 'Company',
+      type: 'string',
+    },
+    {
+      name: 'imageurl', //picture of the person leaving the testimonial
+      title: 'ImageURL',
+      type: 'image',
+      options: {
+        hotspot: true, //for cropping
+      },
+    },
+    {
+        name: 'feebback',
+        title: 'Feedback',
+        type: 'string',
+    }
+  ],
+}
