b00b120e9b3bc33669c0f76e15f74bb51e8af9e2
WitchDevelops <d.n.wojewska@gmail.com>
2025-02-14 17:34:48 +0100

    install pdf viewer library

--- frontend_react/src/components/pdfViewer/pdfViewer.tsx
+++ frontend_react/src/components/pdfViewer/pdfViewer.tsx
@@ -1,24 +1,32 @@
 import React from "react";
-import styled from "styled-components";
+import { Worker, Viewer, ProgressBar } from "@react-pdf-viewer/core";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import { Heading } from "../typography/Heading";
 
 interface PDFViewerProps {
   fileSrc: string;
 }
 
-const StyledPDFViewer = styled.div`
-  width: 100%;
-  height: calc(100vh - 80px);
-  iframe {
-    width: 100%;
-    height: 100%;
-    border: none;
-  }
-`;
-
 export const PDFViewer: React.FC<PDFViewerProps> = ({ fileSrc }) => {
   return (
-    <StyledPDFViewer>
-      <iframe src={fileSrc} />
-    </StyledPDFViewer>
+    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+      <section className="app__wrap">
+        <div style={{ marginBlock: "2rem" }}>
+          <Heading text="My" highlight="Resume" />
+        </div>
+
+        <Viewer
+          fileUrl={fileSrc}
+          renderLoader={(percentages: number) => (
+            <div style={{ width: "240px", height: "300px" }}>
+              <ProgressBar progress={Math.round(percentages)} />
+            </div>
+          )}
+          theme={{
+            theme: "auto",
+          }}
+        />
+      </section>
+    </Worker>
   );
 };
