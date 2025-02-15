f09158751393d608dbcb6b3b4413a32775d54d1b
WitchDevelops <d.n.wojewska@gmail.com>
2025-02-15 20:06:30 +0100

    hide sanity token

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -1,13 +1,12 @@
 //to  connect backed with frontend
-import { createClient } from '@sanity/client';
-import imageUrlBuilder from '@sanity/image-url';
+import { createClient } from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
-    projectId: process.env.VITE_SANITY_PROJECT_ID,
-    dataset: 'production',
-    apiVersion: '2022-03-10',
-    useCdn: true,
-    token: process.env.VITE_SANITY_TOKEN,
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
+  dataset: "production",
+  apiVersion: "2022-03-10",
+  useCdn: true,
 });
 
 const builder = imageUrlBuilder(client);
